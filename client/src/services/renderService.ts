343586968b9af475dc41c4066ce87c09e7b87a83
Boris <emplonics@gmail.com>
2023-07-30 00:33:46 +0300

    building windows and alternative way to draw a building

--- client/src/services/renderService.ts
+++ client/src/services/renderService.ts
@@ -3,13 +3,17 @@ import {
   Engine,
   UniversalCamera,
   HemisphericLight,
+  Vector2,
   Vector3,
   VertexData,
   MeshBuilder,
+  CSG,
+  PolygonMeshBuilder,
   Color3,
   Mesh,
   StandardMaterial,
 } from "@babylonjs/core";
+import earcut from "earcut";
 import { castTo3DCoordinates, type Rotation } from "@/helpers/geometryHelper";
 import { Building } from "@/types/buildings";
 import type { Vector } from "@/types";
@@ -36,7 +40,7 @@ export class RenderService {
       this.scene
     );
     new HemisphericLight("light", new Vector3(0, 1, 0), this.scene);
-    // camera.attachControl(canvas, true);
+    this.camera.attachControl(canvas, true);
 
     const ground = MeshBuilder.CreateGround("ground", {
       width: 10,
@@ -58,6 +62,7 @@ export class RenderService {
       width: meshCellSide,
       height: meshCellSide * this.PERSON_HEIGHT,
       depth: meshCellSide,
+      sideOrientation: Mesh.DOUBLESIDE,
     });
   }
   static removeMesh(mesh: Mesh) {
@@ -95,19 +100,25 @@ export class RenderService {
   }
 
   addBuilding(building: Building) {
-    let w;
     const walls = building.walls.map(wall => {
       const { x, y, z } = castTo3DCoordinates(this.getCoordinates(wall.corner));
       return {
         ...wall,
         corner: new Vector3(x, y, z),
+        windows: wall.windows.map(wallWindow => ({
+          left: wallWindow.left / this.fieldSize.width,
+          width: wallWindow.width / this.fieldSize.width,
+          base: wallWindow.base / this.fieldSize.width,
+          height: wallWindow.height / this.fieldSize.width,
+        })),
       };
     });
 
-    const ply = walls[0].thick;
-    const height = walls[0].height;
+    const { thick: wallThickness, height } = walls[0];
+    const wallsCount = walls.length;
 
-    const outerData = [];
+    const outerCornersPoints: Array<Vector3> = [];
+
     let angle = 0;
     let direction = 0;
     let line = Vector3.Zero();
@@ -115,86 +126,137 @@ export class RenderService {
     walls[1].corner.subtractToRef(walls[0].corner, line);
     const nextLine = Vector3.Zero();
     walls[2].corner.subtractToRef(walls[1].corner, nextLine);
-    const nbWalls = walls.length;
-    for (w = 0; w <= nbWalls; w++) {
+
+    for (let w = 0; w <= wallsCount; w++) {
       angle = Math.acos(
         Vector3.Dot(line, nextLine) / (line.length() * nextLine.length())
       );
       direction = Vector3.Cross(nextLine, line).normalize().y;
       const lineNormal = new Vector3(line.z, 0, -1 * line.x).normalize();
       line.normalize();
-      outerData[(w + 1) % nbWalls] = walls[(w + 1) % nbWalls].corner
-        .add(lineNormal.scale(ply))
-        .add(line.scale((direction * ply) / Math.tan(angle / 2)));
+      outerCornersPoints[(w + 1) % wallsCount] = walls[
+        (w + 1) % wallsCount
+      ].corner
+        .add(lineNormal.scale(wallThickness))
+        .add(line.scale((direction * wallThickness) / Math.tan(angle / 2)));
       line = nextLine.clone();
-      walls[(w + 3) % nbWalls].corner.subtractToRef(
-        walls[(w + 2) % nbWalls].corner,
+      walls[(w + 3) % wallsCount].corner.subtractToRef(
+        walls[(w + 2) % wallsCount].corner,
         nextLine
       );
     }
 
-    const positions = [];
-    const indices = [];
+    const outerCorners: Array<Vector2> = [];
+    const innerCorners: Array<Vector2> = [];
 
-    for (w = 0; w < nbWalls; w++) {
-      positions.push(walls[w].corner.x, walls[w].corner.y, walls[w].corner.z); // inner corners base
-    }
-
-    for (w = 0; w < nbWalls; w++) {
-      positions.push(outerData[w].x, outerData[w].y, outerData[w].z); // outer corners base
-    }
-
-    for (w = 0; w < nbWalls; w++) {
-      indices.push(
-        w,
-        (w + 1) % nbWalls,
-        nbWalls + ((w + 1) % nbWalls),
-        w,
-        nbWalls + ((w + 1) % nbWalls),
-        w + nbWalls
+    for (let w = 0; w < wallsCount; w++) {
+      innerCorners.push(new Vector2(walls[w].corner.x, walls[w].corner.z));
+      outerCorners.push(
+        new Vector2(outerCornersPoints[w].x, outerCornersPoints[w].z)
       );
     }
 
-    let currentLength = positions.length; // inner and outer top corners
-    for (w = 0; w < currentLength / 3; w++) {
-      positions.push(positions[3 * w]);
-      positions.push(height);
-      positions.push(positions[3 * w + 2]);
-    }
+    const exterior = new PolygonMeshBuilder(
+      "exterior",
+      outerCorners,
+      this.scene,
+      earcut
+    );
+    exterior.addHole(innerCorners);
+    const buildingMesh = exterior.build(false, height);
+    buildingMesh.position.y = height;
 
-    currentLength = indices.length;
-    for (let i = 0; i < currentLength / 3; i++) {
-      indices.push(
-        indices[3 * i + 2] + 2 * nbWalls,
-        indices[3 * i + 1] + 2 * nbWalls,
-        indices[3 * i] + 2 * nbWalls
-      );
-    }
+    const buildingForm = CSG.FromMesh(buildingMesh);
+    buildingMesh.dispose();
 
-    for (let w = 0; w < nbWalls; w++) {
-      indices.push(
-        w,
-        w + 2 * nbWalls,
-        ((w + 1) % nbWalls) + 2 * nbWalls,
-        w,
-        ((w + 1) % nbWalls) + 2 * nbWalls,
-        (w + 1) % nbWalls
+    walls.forEach((wall, wallIndex) => {
+      for (const windowPlan of wall.windows) {
+        const nextWallCorner = new Vector3(
+          outerCorners[(wallIndex + 1) % wallsCount].x,
+          0,
+          outerCorners[(wallIndex + 1) % wallsCount].y
+        );
+        const wallCorner = new Vector3(
+          outerCorners[wallIndex].x,
+          0,
+          outerCorners[wallIndex].y
+        );
+
+        const perpendicular = new Vector3(
+          wallCorner.subtract(nextWallCorner).z,
+          0,
+          -wallCorner.subtract(nextWallCorner).x
+        );
+        const nicheDirection = [
+          wallCorner,
+          wallCorner.add(perpendicular.normalize().scale(wallThickness)),
+        ];
+
+        const windowCorners = [
+          [windowPlan.left, windowPlan.base],
+          [windowPlan.left + windowPlan.width, windowPlan.base],
+          [
+            windowPlan.left + windowPlan.width,
+            windowPlan.base + windowPlan.height,
+          ],
+          [windowPlan.left, windowPlan.base + windowPlan.height],
+        ].map(v => new Vector3(v[0], v[1], 0));
+
+        const windowHole = MeshBuilder.ExtrudeShape(
+          "windowHole",
+          {
+            shape: windowCorners,
+            closeShape: true,
+            path: nicheDirection,
+            sideOrientation: Mesh.DOUBLESIDE,
+            cap: Mesh.CAP_ALL,
+          },
+          this.scene
+        );
+        const windowHoleForm = CSG.FromMesh(windowHole);
+        buildingForm.subtractInPlace(windowHoleForm);
+        windowHole.dispose();
+      }
+    });
+
+    return buildingForm.toMesh("building");
+  }
+
+  renderCube(scale: number) {
+    const vertex = [
+      [-0.5, 1, -0.5],
+      [0.5, 1, -0.5],
+      [0.5, 1, 0.5],
+      [-0.5, 1, 0.5],
+      [-0.5, 0, 0.5],
+      [0.5, 0, 0.5],
+      [0.5, 0, -0.5],
+      [-0.5, 0, -0.5],
+    ].map(v => v.map(n => n * scale));
+    const findNeighbour = (v: number[], index: number) =>
+      vertex.findIndex(node =>
+        node.every(
+          (n, i) => (i === index && n !== v[i]) || (i !== index && n === v[i])
+        )
       );
-      indices.push(
-        ((w + 1) % nbWalls) + 3 * nbWalls,
-        w + 3 * nbWalls,
-        w + nbWalls,
-        ((w + 1) % nbWalls) + nbWalls,
-        ((w + 1) % nbWalls) + 3 * nbWalls,
-        w + nbWalls
-      );
-    }
+
+    const positions = vertex.flat();
+    const indices: number[] = [];
+    vertex.forEach((v, i) => {
+      if (indices.includes(i)) {
+        return;
+      }
+      const v1 = findNeighbour(v, 0);
+      const v2 = findNeighbour(v, 1);
+      const v3 = findNeighbour(v, 2);
+      indices.push(i, v1, v2, i, v1, v3, i, v2, v3);
+    });
 
     const normals: number[] = [];
     const uvs: number[] = [];
 
     VertexData.ComputeNormals(positions, indices, normals);
-    VertexData._ComputeSides(Mesh.FRONTSIDE, positions, indices, normals, uvs);
+    VertexData._ComputeSides(Mesh.DOUBLESIDE, positions, indices, normals, uvs);
 
     const customMesh = new Mesh("custom", this.scene);
 
@@ -207,7 +269,17 @@ export class RenderService {
 
     vertexData.applyToMesh(customMesh);
 
-    return customMesh;
+    const hole = MeshBuilder.CreateBox("hole", {
+      width: 0.5,
+      height: 0.5,
+      depth: 0.5,
+    });
+    hole.position = new Vector3(0, 0, -0.5);
+    const frontHole = CSG.FromMesh(hole);
+    const cube = CSG.FromMesh(customMesh);
+    const cubeWithHole = cube.subtract(frontHole);
+
+    return cubeWithHole.toMesh("", null, this.scene);
   }
 
   private getCoordinates(vector: Vector): Vector {
--- client/src/types/buildings.ts
+++ client/src/types/buildings.ts
@@ -1,10 +1,17 @@
 import type { Vector } from "@/types/index";
 
+type WallWindow = {
+  base: number;
+  left: number;
+  width: number;
+  height: number;
+};
 type Wall = {
   corner: Vector;
   width: number;
   height: number;
   thick: number;
+  windows: WallWindow[];
 };
 export type Building = {
   walls: Wall[];
